3bbe433da2a6c603d4a942c5c1b39f66bbd56c7c
kuncoro17 <kuncorochandra17@gmail.com>
2024-06-10 22:59:20 +0700

    web server

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ const renderHTML = (filePath, res) => {
   });
 };
 
+
+
+
 // Middleware untuk melayani file statis dari folder 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
